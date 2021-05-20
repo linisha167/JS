f0c68d9d4c703710fdff021295a9db4fa55d888d
¨linisha¨ <linysha167@hotmail.com>
2021-05-20 20:21:10 -0300

    first_javascript

---
+++ js/basics.js
@@ -0,0 +1,39 @@
+// how to print on console
+
+console.log("hello masao");
+
+// var x = 3;
+// var y = 10;
+//  + concantenation operator
+//  + math operator
+
+
+console.log("x + y = " + (x+y));
+
+console.log("y % x = " +(y % x));
+
+
+if(y < x)
+{
+    console.log( "x is bigger");
+}
+else
+{
+    console.log ("y is bigger");
+
+}
+
+var y = 10;
+var x = 2;
+
+
+
+   
+if ((y % x)==0)
+{
+    console.log ("y is even");
+}
+else
+{
+    console.log ("y is odd ");
+}
